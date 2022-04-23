d7937bed180d12e2a4ccb7725674a9e22f98f99f
horlando <orland20161@gmail.com>
2022-04-23 15:21:50 -0500

    correccion 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  purge: [],
+  purge: [
+    "./pages/**/*.vue",
+    "./components/**/*.vue",
+    "./plugins/**/*.vue",
+    "./static/**/*.vue",
+    "./store/**/*.vue",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        primary: '#031136',
-        secundary: '#ff5903',
-      }
+        primary: "#031136",
+        secundary: "#ff5903",
+      },
     },
   },
   variants: {
